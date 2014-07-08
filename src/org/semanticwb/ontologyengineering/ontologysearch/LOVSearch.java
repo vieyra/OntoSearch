6e1ac09f8ed6277f1566cd5aed0efaef8e55ee8d
Samuel Vieyra <samuel.vieyra@infotec.com.mx>
2014-07-08 11:04:30 -0500

    Commit carried out at April 11 2014

--- src/org/semanticwb/ontologyengineering/ontologysearch/LOVSearch.java
+++ src/org/semanticwb/ontologyengineering/ontologysearch/LOVSearch.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import org.semanticwb.linkeddata.sparql.SPARQLQuery;
+import org.semanticwb.LinkedData.sparql.SPARQLQuery;
 import org.semanticwb.LinkedData.Resources.Prefix.PrefixDeclaration;
 
 /**
